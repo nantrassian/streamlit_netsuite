da4a1f0a565e7e79ead54198f89ea8d9dfe60ea8
nantrassian <160148861+nantrassian@users.noreply.github.com>
2024-02-16 14:25:31 -0500

    Update variables.py

--- functions/variables.py
+++ functions/variables.py
@@ -4,5 +4,5 @@ import streamlit as st
 # Remove options for other databases and schemas
 # Set default values to your BigQuery database and schema
 DATABASE = 'fivetranintegration-393217'
-SCHEMA = 'netsuite_suiteanalytics'
+SCHEMA = 'netsuite_suiteanalytics.'
 DESTINATION = 'BigQuery'

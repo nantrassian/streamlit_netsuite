d684fc37ebf1fc553525c1f5d9e68000b1a7b106
nantrassian <160148861+nantrassian@users.noreply.github.com>
2024-02-16 11:10:21 -0500

    Update netsuite.py

--- netsuite.py
+++ netsuite.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from functions.variables import database_schema_variables, destination_selection
+#from functions.variables import database_schema_variables, destination_selection
 
 st.sidebar.header('Data Connection Variables')
 destination = DESTINATION

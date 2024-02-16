4414821550eb79f877e518eee86d5f073be1da5c
nantrassian <160148861+nantrassian@users.noreply.github.com>
2024-02-16 10:52:33 -0500

    Update 2_balance_sheet_report.py globals

--- pages/2_balance_sheet_report.py
+++ pages/2_balance_sheet_report.py
@@ -7,8 +7,8 @@ from functions.filters import date_filter, filter_data
 from functions.variables import database_schema_variables, destination_selection
 
 st.sidebar.header('Data Connection Variables')
-destination = destination_selection()
-database, schema = database_schema_variables()
+destination = DESTINATION
+database, schema = DATABASE, SCHEMA
 
 st.title('Balance Sheet Dashboard')
 

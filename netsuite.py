b55935a22f19d94e9914ed0411acdd247ff4049e
nantrassian <160148861+nantrassian@users.noreply.github.com>
2024-02-16 10:48:24 -0500

    Update netsuite.py global variables

--- netsuite.py
+++ netsuite.py
@@ -2,8 +2,8 @@ import streamlit as st
 from functions.variables import database_schema_variables, destination_selection
 
 st.sidebar.header('Data Connection Variables')
-destination = destination_selection()
-database, schema = database_schema_variables()
+destination = DESTINATION
+database, schema = DATABASE, SCHEMA
 
 # Read the README contents
 with open("README.md", "r") as f:
